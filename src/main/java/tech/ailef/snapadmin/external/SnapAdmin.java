91704ff34765902679331171f6c57ea2fb3f0a6d
celinemadi-alc <celine.madi@alcumus.com>
2025-09-10 12:49:33 +0300

    Remove the mention of gpl

--- src/main/java/tech/ailef/snapadmin/external/SnapAdmin.java
+++ src/main/java/tech/ailef/snapadmin/external/SnapAdmin.java
@@ -2,18 +2,6 @@
  * SnapAdmin - An automatically generated CRUD admin UI for Spring Boot apps
  * Copyright (C) 2023 Ailef (http://ailef.tech)
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 
@@ -88,7 +76,7 @@ public class SnapAdmin {
 	
 	private boolean authenticated;
 	
-	private static final String VERSION = "0.2.0";
+	private static final String VERSION = "0.2.2-custom";
     
     /**
 	 * Builds the SnapAdmin instance by scanning the `@Entity` beans and loading
