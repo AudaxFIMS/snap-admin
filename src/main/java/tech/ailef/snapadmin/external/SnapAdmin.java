e6f485752799986db67445586dd45229b9e7c934
celinemadi-alc <celine.madi@alcumus.com>
2025-09-10 16:23:25 +0300

    QUAL-5999 revert version name

--- src/main/java/tech/ailef/snapadmin/external/SnapAdmin.java
+++ src/main/java/tech/ailef/snapadmin/external/SnapAdmin.java
@@ -76,7 +76,7 @@ public class SnapAdmin {
 	
 	private boolean authenticated;
 	
-	private static final String VERSION = "0.2.2-custom";
+	private static final String VERSION = "0.2.0";
     
     /**
 	 * Builds the SnapAdmin instance by scanning the `@Entity` beans and loading
